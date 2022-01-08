0f00e55129958e182ec786ee3af74d91fb5468fd
Diogo Alves <diogoalvessilva86@gmail.com>
2022-01-08 12:27:26 -0300

    Evoluindo na lógica para gerar o menor valor de Epsilon a partir dos menos valores dos jogados por seção do campo (incluindo o técnico)

--- teste_mip5.py
+++ teste_mip5.py
@@ -1,4 +1,6 @@
-import sys, heapq, pprint
+import sys, heapq
+from pprint import pprint
+from sys import stdout as out
 from operator import indexOf
 from mip import Model, xsum, maximize, minimize, BINARY, CBC
 import DbConnector
@@ -18,27 +20,40 @@ def FormatarJogadoresPorRodada(jogadores_por_rodada):
     return jogadores_resultado, custo_jogadores_resultado, score_jogadores_resultado
 
 
-def CalcularModelo(rodada, J, c, a, q_i, epsilon):
+def CalcularModelo(rodada, J, c, a, q_i, epsilon, gama_estrutura):
 
     m = Model("Modelo Montagem de Elenco", solver_name=CBC)
     
     #Array com ids das posições dos jogadores
     P = ["tecnico", "goleiro", "zagueiro", "lateral", "meia", "atacante"]
 
-    tecnico = banco.BuscarJogadoresPorRodadaEPosicao(rodada, "tec")
-    goleiro = banco.BuscarJogadoresPorRodadaEPosicao(rodada, "gol")
-    zagueiro = banco.BuscarJogadoresPorRodadaEPosicao(rodada, "zag")
-    lateral = banco.BuscarJogadoresPorRodadaEPosicao(rodada, "lat")
-    meia = banco.BuscarJogadoresPorRodadaEPosicao(rodada, "mei")
-    ataque = banco.BuscarJogadoresPorRodadaEPosicao(rodada, "ata")
+    #tecnico = banco.BuscarJogadoresPorRodadaEPosicao(rodada, "tec")
+    ##valores_tecnico = banco.BuscarJogadoresPorRodadaEPosicao(rodada, "tec")
+    #tecnico_mais_barato = heapq.nsmallest(1, tecnico(2))
+#
+    #goleiro = banco.BuscarJogadoresPorRodadaEPosicao(rodada, "gol")
+    #goleiro_mais_barato = heapq.nsmallest(1, goleiro(2))
+#
+    #zagueiro = banco.BuscarJogadoresPorRodadaEPosicao(rodada, "zag")
+    #zagueiro_mais_barato = heapq.nsmallest(1, zagueiro(2))
+    #
+    #lateral = banco.BuscarJogadoresPorRodadaEPosicao(rodada, "lat")
+    #lateral_mais_barato = heapq.nsmallest(1, lateral(2))
+    #
+    #meia = banco.BuscarJogadoresPorRodadaEPosicao(rodada, "mei")
+    #meia_mais_barato = heapq.nsmallest(1, meia(2))
+    #
+    #ataque = banco.BuscarJogadoresPorRodadaEPosicao(rodada, "ata")
+    #ataque_mais_barato = heapq.nsmallest(1, ataque(2))
 
     #id de todos goleiros, id todos os jogadores de defesa
     #quais jogadores podem jogar em cada posição destas
-    gama = [tecnico, goleiro, zagueiro, lateral, meia, ataque]
+    gama = [gama_estrutura[0], gama_estrutura[1], gama_estrutura[2], gama_estrutura[3], gama_estrutura[4], gama_estrutura[5]]
     
 
     #Variavel de dominio y
     y = [[m.add_var(name='y', var_type=BINARY) for j in range(len(J)) ] for i in range(len(P))]
+
     #y = m.var_by_name('y')
     # #Restricao 3.6
     # for j in range(len(J)):
@@ -74,27 +89,44 @@ def CalcularModelo(rodada, J, c, a, q_i, epsilon):
 
     #Função Objetivo 3.1
     for j in range(len(J)):
-        objetivo = xsum(a[j] * y[i][j] for i in range(len(P)))
+        somatorio_score = xsum(a[j] * y[i][j] for i in range(len(P)))
 
-    m.objective = maximize(objetivo)
+    m.objective = maximize(somatorio_score)
     solucao = m.optimize()
-    return solucao
+    sei_la = []
+    #for j in range(len(J)):
+    #    for i in range(len(P)):
+    #        if y[i][j] == 1:
+    #            sei_la.append(y[i][j])
+    #sel = [i for i in range(len(y)) if y[i].x == 1]
+
+    if m.num_solutions:
+        out.write('objective_value: %g \n' % (m.objective_value))
+    #else:
+    #    print("NÃO ACHOU SOLUÇÃO")
+    #    sys.exit()
+    return m.objective_value
+    #return sei_la
     
 def run(perfis = [], q = [], rodadas = []):
-    #resultado = banco.BuscarJogadoresPorRodadaEPosicao(1, "gol")
-    resultado = banco.BuscarTodosJogadoresPorRodada(1)
-
+    
     solucoes = []
     perfis = [1,2,3,4]
+    limite_rodadas = 2
     #[tecnico, goleiro, zagueiro, lateral, meia, ataque]
     q = [
-        [1,1,2,2,4,2],
+        #[1,1,2,2,4,2],
         [1,1,3,2,3,2]
     ]
     for perfil in perfis:
         # q_i é o esquema tático
         for q_i in q:
-            for rodada in range(1,2):
+            print("COMEÇOU O ESQUEMA TÁTICO: \n" + str(q_i))
+            # Incrementa o limite_rodadas porque o Python não considera o valor limite no laço de repetição
+            for rodada in range(1, limite_rodadas+1):
+
+                print("COMEÇOU A RODADA: \n" + str(rodada))
+
                 # Cartoletas iniciais
                 C = 100
                 # Aqui vai carregar todos os jogadores disponíveis NAQUELA RODADA
@@ -105,9 +137,36 @@ def run(perfis = [], q = [], rodadas = []):
                 """
                 sum(heapq.nsmallest(q_i,c_q0)) + sum(heapq.nsmallest(q[1],c_q1)) + sum(heapq.nsmallest(q[3],c_q3))
                 """
+                gama_estrutura = []
+                
+                tecnico, valores_tecnicos, _ = FormatarJogadoresPorRodada(banco.BuscarJogadoresPorRodadaEPosicao(rodada, "tec"))
+                tecnico_mais_barato = heapq.nsmallest(1, valores_tecnicos)[0]
+                gama_estrutura.append(tecnico)
+
+                goleiro, valores_goleiros, _ = FormatarJogadoresPorRodada(banco.BuscarJogadoresPorRodadaEPosicao(rodada, "gol"))
+                goleiro_mais_barato = heapq.nsmallest(1, valores_goleiros)[0]
+                gama_estrutura.append(goleiro)
+
+                zagueiro, valores_zagueiros, _ = FormatarJogadoresPorRodada(banco.BuscarJogadoresPorRodadaEPosicao(rodada, "zag"))
+                zagueiro_mais_barato = heapq.nsmallest(1, valores_zagueiros)[0]
+                gama_estrutura.append(zagueiro)
+                
+                lateral, valores_laterais, _ = FormatarJogadoresPorRodada(banco.BuscarJogadoresPorRodadaEPosicao(rodada, "lat"))
+                lateral_mais_barato = heapq.nsmallest(1, valores_laterais)[0]
+                gama_estrutura.append(lateral)
+                
+                meia, valores_meias, _ = FormatarJogadoresPorRodada(banco.BuscarJogadoresPorRodadaEPosicao(rodada, "mei"))
+                meia_mais_barato = heapq.nsmallest(1, valores_meias)[0]
+                gama_estrutura.append(meia)
+                
+                ataque, valores_ataque, _ = FormatarJogadoresPorRodada(banco.BuscarJogadoresPorRodadaEPosicao(rodada, "ata"))
+                ataque_mais_barato = heapq.nsmallest(1, valores_ataque)[0]
+                gama_estrutura.append(ataque)
                 
                 # Colocar epsilon variando em "min(c) vezes"
-                soma_epsilons = sum(heapq.nsmallest(11,c))
+                # OBS: colocamos 12 porque agora tem o técnico tbm
+                soma_epsilons = tecnico_mais_barato + goleiro_mais_barato + zagueiro_mais_barato + lateral_mais_barato + meia_mais_barato + ataque_mais_barato
+
                 epsilon = soma_epsilons
                 epsilons = []
                 while epsilon < C:
@@ -119,18 +178,18 @@ def run(perfis = [], q = [], rodadas = []):
                         
                     epsilons.append(epsilon) 
                 
-                #print(J)
-                #print(c)
-                #print(a)
-                #print(epsilons, "epsilons")
-                
                 for epsilon in epsilons:
-                    #print(epsilon, "epsilon")
-                    solucao = CalcularModelo(rodada, J, c, a, q_i, epsilon)
+                    solucao = CalcularModelo(rodada, J, c, a, q_i, epsilon, gama_estrutura)
                     solucoes.append(solucao)
-                    #print(min(c), "min(c)")
-                    #print(solucao)
-            pprint.pprint(solucoes[0])
+
+            print("COMEÇA PRINT SOLUÇÕES")
+            pprint(solucoes)
+            print("TERMINA PRINT SOLUÇÕES")
+            #print("COMEÇA PRINT EPSILONS")
+            #pprint(epsilons)
+            #print("TERMINA PRINT EPSILONS")
+
+            
             sys.exit()
 run()
 
